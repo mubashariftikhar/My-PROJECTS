a21d34637be2d9d0affe1bc8f020f80082ae51c6
Mubashar Iftikhar <62559139+mubashariftikhar@users.noreply.github.com>
2023-10-11 02:33:02 -0700

    Dice Pig Game  using (HTML,CSS,JS)

---
+++ Dice Game/assets/javascripts/script.js
@@ -0,0 +1,100 @@
+'use strict';
+
+// selecting Elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
+const diceEl = document.querySelector('.dice');
+
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+let scores, currentScore, activePlayer, playing;
+// Starting condition
+
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove(`player--winner`);
+  player1El.classList.remove(`player--winner`);
+  player0El.classList.add(`player--active`);
+  player1El.classList.remove(`player--active`);
+};
+init();
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent =
+    currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle(`player--active`);
+  player1El.classList.toggle(`player--active`);
+};
+
+// Rolling Dice functionality
+btnRoll.addEventListener('click', function () {
+  if (playing) {
+    // 1.Generating a random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    console.log(dice);
+
+    //2. display dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `./assets/images/dice-${dice}.png`;
+
+    //3. check for rolled 1: if true, switch to next player
+    if (dice !== 1) {
+      // add dice to current score
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+      // current0El.textContent = currentScore;
+    } else {
+      // switch to next player
+      switchPlayer();
+    }
+  }
+});
+// HOLD  BUTTON
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    // 1. add cureent score to active players score
+    scores[activePlayer] += currentScore; //scores[1] = scores[1] + currentScore;
+
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+
+    // 2. check if player score is >=100
+    if (scores[activePlayer] >= 20) {
+      //finish the game
+      playing = false;
+      diceEl.classList.add('hidden');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add(`player--winner`);
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove(`player--active`);
+    } else {
+      // switch to the next palyer
+      switchPlayer();
+    }
+  }
+});
+// NEW GAME---BUTTON
+btnNew.addEventListener('click', init);
